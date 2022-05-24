5562f8da46fa3cad7f41a8a2262e1c07ca5c0a0a
Daniel Bedoya <arturo.lpc12@gmail.com>
2022-05-23 21:51:26 -0500

    Implement Navigation

---
+++ app/src/main/java/com/example/truelogicappchallenge/global/Screen.kt
@@ -0,0 +1,28 @@
+package com.example.truelogicappchallenge.global
+
+import com.example.truelogicappchallenge.presentation.model.CharacterView
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+sealed class Screen(val route: String) {
+    object MainScreen: Screen("main_screen")
+    object DetailScreen: Screen("detail_screen")
+
+    fun withArgs(vararg args: String): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+    }
+
+    fun withArgsDiffType(characterView: CharacterView ): String {
+        return buildString {
+            val urlEncoded = URLEncoder.encode(characterView.img, StandardCharsets.UTF_8.toString())
+            val partialString = withArgs(characterView.name, characterView.nickname, urlEncoded)
+            append(partialString)
+            append("/${characterView.isFavorite}")
+        }
+    }
+}
--- app/src/main/java/com/example/truelogicappchallenge/presentation/ui/MainComposeActivity.kt
+++ app/src/main/java/com/example/truelogicappchallenge/presentation/ui/MainComposeActivity.kt
@@ -11,23 +11,32 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.*
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.example.truelogicappchallenge.R
+import com.example.truelogicappchallenge.global.MainScreen
+import com.example.truelogicappchallenge.global.Navigation
+import com.example.truelogicappchallenge.global.Screen
 import com.example.truelogicappchallenge.presentation.model.CharacterView
 import com.example.truelogicappchallenge.presentation.ui.ui.theme.TruelogicAppChallengeTheme
 import com.example.truelogicappchallenge.presentation.viewmodel.ListCharactersViewModel
@@ -45,7 +54,9 @@ class MainComposeActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MainContainer()
+                    //MainContainer()
+                    //Navigation()
+                    MainWithNavigation()
                 }
             }
         }
@@ -60,13 +71,136 @@ fun DefaultPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
-            ErrorTextComponent("Redbee")
+            //Navigation()
+            //ErrorTextComponent("Redbee")
+            DetailScreenComponent(
+                "name",
+                "nickname",
+                "https://s-i.huffpost.com/gen/1317262/images/o-ANNA-GUNN-facebook.jpg",
+                true)
+            /*CircularProgressIndicator(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .requiredSize(60.dp))*/
         }
     }
 }
 
 @Composable
-fun MainContainer(listCharactersViewModel: ListCharactersViewModel = viewModel()){
+fun MainWithNavigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+        composable(
+            route = Screen.MainScreen.route) {
+            val listCharactersViewModel = hiltViewModel<ListCharactersViewModel>()
+            MainContainer(navController = navController, listCharactersViewModel)
+        }
+        composable(
+            route = Screen.DetailScreen.route + "/{name}/{nickname}/{img}/{isFavorite}",
+            arguments = listOf(
+                navArgument("name") {
+                    type = NavType.StringType
+                    defaultValue = ""
+                    nullable = false
+                },
+                navArgument("nickname") {
+                    type = NavType.StringType
+                    defaultValue = ""
+                    nullable = false
+                },
+                navArgument("img") {
+                    type = NavType.StringType
+                    defaultValue = ""
+                    nullable = false
+                },
+                navArgument("isFavorite") {
+                    type = NavType.BoolType
+                    defaultValue = false
+                    nullable = false
+                }
+            )
+        ) {
+            navBackStackEntry ->
+            DetailScreenComponent(
+                name = navBackStackEntry.arguments?.getString("name") ?: "",
+                nickname = navBackStackEntry.arguments?.getString("nickname") ?: "",
+                img = navBackStackEntry.arguments?.getString("img") ?: "",
+                isFavorite = navBackStackEntry.arguments?.getBoolean("isFavorite") ?: false
+            )
+        }
+
+    }
+}
+
+@Composable
+fun DetailScreenComponent(name: String, nickname: String, img: String, isFavorite: Boolean){
+    /*Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(12.dp)) {
+            Text(text = name, textAlign = TextAlign.Center)
+            Text(text = nickname, textAlign = TextAlign.Center)
+            Text(text = img, textAlign = TextAlign.Center)
+            Text(text = "The flag is $isFavorite", textAlign = TextAlign.Center)
+        }
+    }*/
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 20.dp),
+        verticalAlignment = Alignment.Top
+    ) {
+        GlideImage(
+            imageModel = img,
+            requestOptions = {
+                RequestOptions()
+                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+            },
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .requiredSize(width = 140.dp, height = 200.dp)
+        )
+        Column(
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier
+                .weight(1f)
+                .padding(vertical = 20.dp)
+        ) {
+            Text(
+                text = name,
+                textAlign = TextAlign.Start,
+                fontSize = 26.sp
+            )
+            Text(
+                text = nickname,
+                textAlign = TextAlign.Start,
+                fontSize = 20.sp
+            )
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = if(isFavorite) Companion.Red else Companion.Blue
+                ),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(
+                    text = if (isFavorite) "Remove from favorites" else "Add to favorites",
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MainContainer(navController: NavController, listCharactersViewModel: ListCharactersViewModel){
 
     val lazyState = rememberLazyListState()
 
@@ -81,7 +215,7 @@ fun MainContainer(listCharactersViewModel: ListCharactersViewModel = viewModel()
     }
 
     if(container == true && emptyView == false && progressBar == false) {
-        CharactersListComponent(lazyState, list, listCharactersViewModel)
+        CharactersListComponent(navController, lazyState, list, listCharactersViewModel)
     }
 
     if(emptyView == true && container == false && progressBar == false){
@@ -96,7 +230,10 @@ fun MainContainer(listCharactersViewModel: ListCharactersViewModel = viewModel()
 @Composable
 fun ProgressBarComponent(){
     CircularProgressIndicator(
-        modifier = Modifier.requiredSize(60.dp))
+        modifier = Modifier
+            .fillMaxSize()
+            .requiredSize(60.dp)
+            .wrapContentHeight(Alignment.CenterVertically))
 }
 
 @Composable
@@ -104,6 +241,7 @@ fun ErrorTextComponent(errorMessage: String){
     Text(
         text = errorMessage,
         modifier = Modifier
+            .fillMaxSize()
             .wrapContentHeight(Alignment.CenterVertically)
             .padding(horizontal = 20.dp),
         textAlign = TextAlign.Center)
@@ -111,7 +249,7 @@ fun ErrorTextComponent(errorMessage: String){
 
 
 @Composable
-fun CharactersListComponent(lazyState: LazyListState, list: List<CharacterView>?, listCharactersViewModel: ListCharactersViewModel) {
+fun CharactersListComponent(navController: NavController, lazyState: LazyListState, list: List<CharacterView>?, listCharactersViewModel: ListCharactersViewModel) {
 
     LazyColumn(
         contentPadding = PaddingValues(vertical = 8.dp),
@@ -125,7 +263,9 @@ fun CharactersListComponent(lazyState: LazyListState, list: List<CharacterView>?
                     nickname = characterView.nickname,
                     img = characterView.img,
                     isFavorite = characterView.isFavorite
-                )
+                ), {
+                    navController.navigate(Screen.DetailScreen.withArgsDiffType(characterView))
+                }
             ) {
                 listCharactersViewModel.updateFavoriteStatus(index)
             }
@@ -136,18 +276,20 @@ fun CharactersListComponent(lazyState: LazyListState, list: List<CharacterView>?
 
 
 @Composable
-fun CharacterItemComponent(characterView: CharacterView, callbackItem: () -> Unit) {
+fun CharacterItemComponent(characterView: CharacterView, navCallback: () -> Unit , callbackItem: () -> Unit) {
     Row(
         modifier = Modifier
             .background(MaterialTheme.colors.background)
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .clickable {
+                navCallback()
+            },
         verticalAlignment = Alignment.CenterVertically
     ) {
         GlideImage(
             imageModel = characterView.img,
             requestOptions = {
                 RequestOptions()
-                    .override(256, 256)
                     .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
                     .centerCrop()
             },
@@ -162,9 +304,9 @@ fun CharacterItemComponent(characterView: CharacterView, callbackItem: () -> Uni
                 .weight(3f)
                 .padding(start = 8.dp)
         ) {
-
             Text(
                 text = characterView.name,
+                textAlign = TextAlign.Start,
                 modifier = Modifier
                     .background(MaterialTheme.colors.background)
                     .fillMaxWidth()
@@ -172,6 +314,7 @@ fun CharacterItemComponent(characterView: CharacterView, callbackItem: () -> Uni
             )
             Text(
                 text = characterView.nickname,
+                textAlign = TextAlign.Start,
                 modifier = Modifier
                     .background(MaterialTheme.colors.background)
                     .fillMaxWidth()
